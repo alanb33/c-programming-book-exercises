982d6abad42614eeba1e32d1a629a3fafcfbf43f
alanb33 <ab33@protonmail.com>
2025-05-20 14:41:52 -0400

    2-3 Part 3: Calculations (now completed)

--- 2/2-3-htoi.c
+++ 2/2-3-htoi.c
@@ -1,15 +1,15 @@
 /*
     htoi.c
 
-    Alan Bailey, May 19, 2025
+    Alan Bailey, May 20, 2025
 
     Given a string in the format 0xNNNNNNNN, where N is one or more hex
     digits in the range of 0 to F, convert to integer equivalent. 0x/0X are
     an optional component and it should work with the following:
 
-    0x12345678  
-    0x1234
-    1234
+    0x12345678      outputs 305419896
+    0x1234          outputs 4660
+    1234            outputs 4660
 
     Note to self - hex is like binary and read right to left. Like octal as
     well, the value is calculated by value^place, starting at 0.
@@ -18,10 +18,13 @@
 /*
     Abstract
 
+    POSTMORTEM NOTE: The abstract was not fully accurate to the resulting
+        program, but at least provided a useful starting point. -A
+
     vars
         ending_index    the index to stop reading at. default 0.
         max_length      the maxiumum length of the string. default 8.
-        place           the exponent to use against the value. default 0.
+        exp             the exponent to calculate hex with. default 0.
         product         the running product of the calculation. default 0.
 
     1. Receive a string.
@@ -45,8 +48,8 @@
             determined by subtracting char 0 from them. capital letters are
             calcuated with letter - 'A' + 10. lowercase letters instead use
             letter - 'a' + 10.
-        b. Get the product of pow(value, place). Sum into product.
-        c. Decrement index and increment place.
+        b. Get the product of value * pow(base, exp). Sum into product.
+        c. Decrement index and increment exp.
         d. If index < ending_index, we're done!
 */
 
@@ -55,9 +58,13 @@
 #include <string.h>
 
 #define MAX_LEN     10  /* Max length of string */
+#define HEX_BASE    16
+
 #define DO_TESTS    1   /* 1 to run tests, 0 otherwise */
 
 void get_line(char s[]);
+unsigned long calculate_hex(char s[]);
+int calculate_value(char c);
 int validate_hex(char s[]);
 
 // Test functions
@@ -74,8 +81,7 @@ int main(void) {
     get_line(s);
 
     if (validate_hex(s) == 0) {
-        printf("Hex is valid\n");
-        // calculate_hex(s)
+        printf("The value of the provided hex is %lu.\n", calculate_hex(s));
     } else {
         printf("The specified string is invalid as a hexidecimal input. It "
             "must be presented in the format of: \n\n\t[0x]NNNNNNNN\n\nWhere "
@@ -85,6 +91,74 @@ int main(void) {
 
     return 0;
 };
+
+/*
+    int calculate_hex(char s[])
+
+    From a given hexadecimal string in the format NNNNNNNN or 0xNNNNNNNN where
+    N is any hexademical value, 0-9/A-F, case-insensitive, calculate the 
+    equivalent decimal value.
+
+    Input s is expected to be validated before calculation. validate_hex(s)
+    should be run before the execute of calculate_hex(s).
+
+    Returns the decimal value of the hex input.
+*/
+unsigned long calculate_hex(char s[]) {
+    int end_i = 0;
+    if (s[1] == 'x' || s[1] == 'X') {
+        end_i = 2;          // We have a 0x preface, so move the end_i
+    }
+
+    size_t len = strlen(s);
+    int i = len - 1;  // Get the "starting" index at the end of s
+    int exp = 0;
+    unsigned long value = 0;
+    while (i >= end_i) {
+        long decimal = calculate_value(s[i]) * pow(16, exp);
+        value = value + decimal;
+        ++exp;
+        --i;
+    }
+
+    return value;
+}
+
+/*
+    int calculate_value(char c)
+
+    Given a valid hexademical digit c, return its decimal value.
+
+    This function expects to receive valid hexes and performs no validation on
+    its own. Expected order of operations is to receive a hex string, then 
+    validate that hex string, before calling calculate_hex, which calls 
+    calculate_value on its own. Calculate value can return a value without the
+    assistance of calculate_hex, but input should be validated before it's
+    passed to this function.
+
+    PS: Has the book touched switches yet? Not sure -- I'm aware of them from
+    other languages, though. -A
+*/
+int calculate_value(char c) {
+    int value = -1;
+
+    if (c >= '0' && c <= '9') {
+        value = (c - '0');
+    }
+
+    // a's place is 97, and hex a value is 10, so c - 'a' + 10.
+    if (c >= 'a' && c <= 'f') {
+        value = (c - 'a' + 10);
+    }
+
+    // As above, but A is value 65, so we need a different case to handle it.
+    if (c >= 'A' && c <= 'F') {
+        value = (c - 'A' + 10);
+    }
+
+    return value;
+}
+
 
 void get_line(char s[]) {
     /*
@@ -167,6 +241,8 @@ int run_tests(void) {
 
     int failure = 0;
     int test_count = 0;
+
+    /*                          HEX INPUT VALIDATION SECTION                  */
 
     /*          INVALID HEX LENGTH           */
     /*          With 0x header               */
@@ -261,7 +337,74 @@ int run_tests(void) {
         failure = fail_test("Test failure: Case 6: Mixed case, no header was not accepted.\n", failure);
     }
     ++test_count;
-  
+
+    /*                          HEX VALUE VALIDATION SECTION                  */
+
+    // We only need to test valid hexes here.
+    char hexes[HEX_BASE] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A',
+        'B', 'C', 'D', 'E', 'F'};
+
+    for (int i = 0; i < HEX_BASE; i++) {
+        int result = calculate_value(hexes[i]);
+        if (result != i) {
+            // Special failure case with formatting. oops!
+            printf("Test failure: %c does not equal %d, but instead %d.\n", 
+                hexes[i], i, result);
+            ++failure;
+        }
+        ++test_count;
+    }
+
+    char hexes_lower[HEX_BASE - 10] = {'a', 'b', 'c', 'd', 'e', 'f'};
+
+    for (int i = 0; i < HEX_BASE - 10; i++) {
+        int expected = i + 10;
+        int result = calculate_value(hexes_lower[i]);
+        if (result != expected) {
+            // Special failure case with formatting. oops!
+            printf("Test failure: %c does not equal %d, but instead %d.\n", 
+                hexes_lower[i], expected, result);
+            ++failure;
+        }
+        ++test_count;
+    }
+
+    // Full hex strings
+    // Edges
+    if (calculate_hex("0xFFFFFFFF") != 4294967295) {
+        failure = fail_test("Test failure: 0xFFFFFFFF does not equate to 4294967295\n", failure);
+    }
+    ++test_count;
+
+    if (calculate_hex("0x0") != 0) {
+        failure = fail_test("Test failure: 0x0 does not equate to 0\n", failure);
+    }
+    ++test_count;
+
+    // Arbitrary
+    if (calculate_hex("0x0A9D") != 2717) {
+        failure = fail_test("Test failure: 0x0A9D does not equate to 2717\n", failure);
+    }
+    ++test_count;
+
+    // yum yum
+    if (calculate_hex("0xDEADBEEF") != 3735928559) {
+        failure = fail_test("Test failure: 0x0 does not equate to 3735928559\n", failure);
+    }
+    ++test_count;
+
+    if (calculate_hex("0x1010DaB0") != 269540016) {
+        failure = fail_test("Test failure: 0x1010DaB0 does not equate to 269540016\n", failure);
+    }
+    ++test_count;
+
+    if (calculate_hex("7302D9C") != 120597916) {
+        failure = fail_test("Test failure: 7302D9C does not equate to 120597916\n", failure);
+    }
+    ++test_count;
+
+
+    // Test results
 
     if (failure > 0) {
         printf("%d tests of %d tests failed.\n", failure, test_count);
