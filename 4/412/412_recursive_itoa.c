efd0baa7ea658569f91674056c10d813667714f1
alanb33 <ab33@protonmail.com>
2025-05-30 18:20:21 -0400

    Exercise 4-12: Recrusive itoa

---
+++ 4/412/412_recursive_itoa.c
@@ -0,0 +1,67 @@
+/* 
+    recursive_itoa.c
+
+    Alan Bailey
+    May 30, 2025
+    Exercise 4-12
+
+    Write a recursive version of itoa that converts an integer into a string
+    by calling a recursive routine.
+
+    Extra: I modified this to work with the largest negative number as well.
+        -A
+*/
+
+#include <limits.h>
+#include <stdio.h>
+
+void itoa_recursive(int n);
+
+int main(void) {
+    itoa_recursive(INT_MIN);
+    putchar('\n');
+    itoa_recursive(CHAR_MIN);
+    putchar('\n');
+    return 0;
+}
+
+/*
+    itoa_recursive
+
+    Given any integer n, print its digits as characters, including the
+    negative sign of the digit if it exists.
+
+    n is a register because it's used a few times. Probably unncessary!
+        -A
+*/
+void itoa_recursive(register int n) {
+    char sign = n < 0 ? -1 : 1;
+
+    // Preface the number with a minus if it's negative
+    if (n < 0) {
+        putchar('-');
+    }
+
+    // The digit formula will get the modulo and then make it positive.
+    char digit = (sign * (n % (sign * 10))) + '0';
+    
+    // As long as n / 10 isn't 0, continue with a new n!
+    if (n / 10) {
+        int div_n = n / 10;
+
+        /*
+            div_n will only ever be negative on the first invocation of
+            the function. This makes it safe to use with the largest
+            negative numbers and avoids having to use a separate variable
+            to track if we're on the first digit or not.
+        */
+        if (div_n < 0) {
+            div_n = -div_n;
+        }
+        
+        itoa_recursive(div_n);
+    }
+    
+    // This section only runs after recursion finished
+    putchar(digit);
+}
